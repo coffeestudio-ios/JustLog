0736bd0c86e21dc9a53edc676ce3cee8de972723
Art <artshesha@protonmail.com>
2021-01-13 18:53:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "JustLog",
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "JustLog", targets: ["JustLog"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .uptonextmajor(from: "1.7.0")),
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", .uptonextmajor(from: "7.6.0"))
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.7.0"),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.3")
     ],
     targets: [
-        .target(name: "JustLog")
+        .target(name: "JustLog", dependencies: ["SwiftyBeaver", "CocoaAsyncSocket"], path: "JustLog")
     ],
     swiftLanguageVersions: [.v5]
 )
